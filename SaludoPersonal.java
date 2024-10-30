3bd1c30715698ef28668ef811436674563a4b3b0
Anyerson Bolivar Zamora Varon <anyersonzamora@hotmail.com>
2024-10-30 11:29:27 -0500

    Ejercicios Clase 02

---
+++ SaludoPersonal.java
@@ -0,0 +1,7 @@
+public class SaludoPersonal {
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Cesar, Bienvenido al curso de Programación en Java");
+    }
+}
